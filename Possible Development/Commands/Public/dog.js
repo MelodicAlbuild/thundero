27c3b270b1d8c403ec206f00607359da0b540dc9
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2019-10-28 17:35:41 -0400

    Command File Time

---
+++ Possible Development/Commands/Public/dog.js
@@ -0,0 +1,19 @@
+const random = require("random-animal");
+
+module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg) => {
+    random.dog().then(url => {
+        msg.channel.createMessage({
+            embed: {
+                author: {
+                    name: bot.user.username,
+                    icon_url: bot.user.avatarURL,
+                    url: "https://echo-studios.co.uk"
+                },
+                color: 0x00FF00,
+                image: {
+                    url: url
+                }
+            }
+        });
+    });
+};
---
+++ Possible Development/Commands/Public/info.js
@@ -0,0 +1,76 @@
+const moment = require("moment");
+
+module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg) => {
+	let embed_fields = [
+		{
+			name: `Guild Name`,
+			value: `__**${msg.channel.guild.name}**__`,
+			inline: true
+		},
+		{
+			name: `🆔`,
+			value: `${msg.channel.guild.id}`,
+			inline: true
+		},
+		{
+			name: `🗓 Created`,
+			value: `${moment(msg.channel.guild.createdAt).fromNow()}`,
+			inline: true
+		},
+		{
+			name: `👑 Owned by`,
+			value: `<@${msg.channel.guild.ownerID}>`,
+			inline: true
+		},
+		{
+			name: "👥",
+			value: `${msg.channel.guild.members.size} members`,
+			inline: true
+		}
+	];
+	let image_url = "";
+	if(msg.channel.guild.iconURL) {
+		image_url = msg.channel.guild.iconURL;
+	}
+	embed_fields.push(
+		{
+			name: `🕯 Command Prefix:`,
+			value: `\`${bot.getCommandPrefix(msg.channel.guild, serverDocument)}\``,
+			inline: true
+		},
+		{
+			name: `💬`,
+			value: `${serverDocument.messages_today} message${serverDocument.messages_today == 1 ? "" : "s"} today`,
+			inline: true
+		},
+		{
+			name: `🗄 Category:`,
+			value: `${serverDocument.config.public_data.server_listing.category}`,
+			inline: true
+		},
+		{
+			name: `🌎`,
+			value: `Click [here](${config.hosting_url}activity/servers?q=${encodeURIComponent(msg.channel.guild.name)})`,
+			inline: true
+		},
+		{
+			name: "🖼",
+			value: `Icon:`,
+			inline: true
+		}
+	);
+	msg.channel.createMessage({
+		embed: {
+            author: {
+                name: bot.user.username,
+                icon_url: bot.user.avatarURL,
+                url: "https://echo-studios.co.uk"
+            },
+            color: 0x00FF00,
+			fields: embed_fields,
+			image: {
+				url: image_url
+			}
+		}
+	});
+};
